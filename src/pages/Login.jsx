916058a04e1b231c7754272b22af333b08c08fb6
Khalid <khankhalid1743@gmail.com>
2022-12-22 19:17:33 +0530

    Added Login and Register Pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import './login.css'
+import {GrInstagram} from "react-icons/gr"
+import { useState } from "react";
+
+
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  console.log("Submit Clicked")
+}
+
+
+
+
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ 
+return <div className="login__page">
+    <div className="login__window">
+      <form onSubmit={handleSubmit}>
+        <div className="logo__div"><GrInstagram className="ig__logo"/><span>Instagram</span></div>
+        <div className="input__div">
+          <span>Email</span>
+        <input type="email" onKeyDown={(e)=>{setEmail(e.target.value)}} placeholder="Email" />
+        </div>
+        <div className="input__div">
+        <span>Password</span>
+        <input type="password" onKeyDown={(e)=>{setPassword(e.target.value)}} placeholder="Password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      </div></div>;
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import './register.css'
+import {GrInstagram} from "react-icons/gr"
+import { useState } from "react";
+
+
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  console.log("Submit Clicked")
+}
+
+
+
+
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true)
+  const handleChange = () => {
+    setDisabled(disabled? (false):(true))
+  }
+
+return <div className="login__page">
+    <div className="login__window">
+      <form onSubmit={handleSubmit}>
+        <div className="logo__div"><GrInstagram className="ig__logo"/><span>Instagram</span></div>
+        <div className="input__div">
+          <span>Email</span>
+        <input type="email" onKeyDown={(e)=>{setEmail(e.target.value)}} placeholder="Email" />
+        </div>
+        <div className="input__div">
+        <span>Password</span>
+        <input type="password" onKeyDown={(e)=>{setPassword(e.target.value)}} placeholder="Password" />
+        </div>
+        <div className="terms__div">
+          <input onChange={handleChange} type="checkbox" /><span>Accept terms and conditions</span></div>
+        <button className={disabled? (''):("notdisabled")} disabled={disabled} type="submit">Register</button>
+      </form>
+      </div></div>;
 };
 
 export default Register;
